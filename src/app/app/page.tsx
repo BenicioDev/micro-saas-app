99230c98983122b397745ea8b60d4d277299e5eb
Gabriel Benicio <gabrielvitor083@gmail.com>
2024-03-01 18:51:39 -0300

    add routes

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,9 @@
-export default function Page(){
+import { auth } from "@/services/auth"
+
+
+export default async function Page(){
+       const session = await auth()
        return(
-              <h1>App Page</h1>
+              <h1>{session?.user?.email}</h1>
        )
 }
